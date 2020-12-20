284ed3a48f811e42300c6592e35249a3642b8b15
FraCav99 <MasterCaster36@outlook.it>
2020-12-20 11:30:33 +0100

    Modify backend logic, add section for show stats and modify relative stylesheets

--- app.js
+++ app.js
@@ -11,27 +11,28 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public/'));
 
 
 app.route('/')
     .get((req, res) => res.render('index'))
-    .post((req, res) => {
-        const city = req.body.cityInput;
-        const cityName = city.charAt(0).toUpperCase() + city.slice(1);
-        const openweathermapAPI_KEY = process.env.OPENWEATHER_API_KEY;
-        const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${openweathermapAPI_KEY}`;
-        
-        axios
-            .get(url)
-            .then(response => {
-                console.log(response.data);
-                res.render('index', {weatherData: response.data});
-            })
-            .catch(error => {
-                console.log(error);
-                res.render('index', {error: 'An error occurred!'});
-            });
+    .post(async (req, res) => {
+        try {
+            const city = req.body.cityInput;
+            const cityName = city.charAt(0).toUpperCase() + city.slice(1);
+            
+            const openweathermapAPI_KEY = process.env.OPENWEATHER_API_KEY;
+            const giphyAPI_KEY = process.env.GIPHY_API_KEY;
+            
+            const weatherURL = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${openweathermapAPI_KEY}`;
+            
+            const weatherResponse = await axios.get(weatherURL);
+            const giphyResponse = await axios.get(`https://api.giphy.com/v1/gifs/translate?api_key=${giphyAPI_KEY}&s=${weatherResponse.data.weather[0].main}`);
+
+            res.render('index', {weatherData: weatherResponse.data, giphyData: giphyResponse.data.data.images.original.url})
+        } catch (error) {
+            res.render('index', {error: 'An error occurred!'});
+        }
     });
 
 
